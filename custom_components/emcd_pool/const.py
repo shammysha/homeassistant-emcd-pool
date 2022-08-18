0f61d5f9fc710c59cc1369c9dab8842a08cb7bde
Maxim Sharenkov <mcshaman@gmail.com>
2022-08-18 15:12:19 +0300

    Fix Schemas

--- custom_components/emcd_pool/const.py
+++ custom_components/emcd_pool/const.py
@@ -1,6 +1,6 @@
 from typing import Final
 
-VERSION: Final = "2.0.3"
+VERSION: Final = "2.0.4"
 FLOW_VERSION = "1" 
 
 DOMAIN: Final = "emcd_pool"
--- custom_components/emcd_pool/schemas.py
+++ custom_components/emcd_pool/schemas.py
@@ -14,12 +14,8 @@ from .const import (
 
 CONFIG_ENTRY_SCHEMA = vol.Schema(
     {
-        DOMAIN: vol.Schema(
-            {
-                vol.Required(CONF_API_KEY): cv.string,
-                vol.Required(CONF_USERNAME): cv.string
-            }
-        )
+        vol.Required(CONF_API_KEY): cv.string,
+        vol.Required(CONF_USERNAME): cv.string
     },
     extra=vol.PREVENT_EXTRA
 ) 
